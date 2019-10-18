e4226750a9cf08ade327d62466308e8e0971e62d
K00229022 <K00229022@student.lit.ie>
2019-10-18 11:10:03 +0100

    Effects to Booby Trap

---
+++ Assets/SampleScenes/Materials/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerObject;
+    void Update()
+    {
+        Vector3 lookOnObject = playerObject.position - transform.position;
+        lookOnObject = playerObject.position - transform.position;
+        transform.forward = lookOnObject.normalized;
+    }
+}
---
+++ Assets/TriggerBoxToFall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBoxToFall : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject cube;
+
+    void Start()
+    {
+        //cube.GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        cube.GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
